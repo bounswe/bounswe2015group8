e97989f0d3fbf4aeb5fff159bf373bff9cfbc85f
Goktug Erce Gurel <goktugercegurel@gmail.com>
2015-05-02 21:53:55 +0300

    Add multiplication operation in MathematicalOperations class and test functions for multiplication operation in MathematicalOperationsTest class.

--- MathematicalOperations.java
+++ MathematicalOperations.java
@@ -1,7 +1,12 @@
 public class MathematicalOperations {
-	
+
 	/* The division function for numbers with double type */
-	public double div(double x, double y){
-		return x/y;
+	public double div(double x, double y) {
+		return x / y;
+	}
+
+	/* The multiplication function for numbers with double type */
+	public double multiply(double x, double y) {
+		return x * y;
 	}
 }
--- TestMathematicalOperations.java
+++ TestMathematicalOperations.java
@@ -13,4 +13,22 @@ public class TestMathematicalOperations{
 		assertEquals("-3.0 divided by 0.0 must be negatively infinite", Double.NEGATIVE_INFINITY, mOp.div(-3.0, 0.0), 0.00000001);
 		assertEquals("0.0 divided by 0.0 must be undefined (NAN)", Double.NaN, mOp.div(0.0, 0.0), 0.00000001);
 	}
+	
+	/* Unit Test for double multiplication in MathematicalOperations class */
+	@Test
+	public void multiplyByZero() {
+		MathematicalOperations multiplier = new MathematicalOperations();
+		
+		// Test cases for multiplying by zero
+		assertEquals("Multiplication of -5 with 0 is 0", 0, multiplier.multiply(-5, 0), 0.00000001);
+		assertEquals("Multiplication of 0 with -0 is 0", 0, multiplier.multiply(0, -0), 0.00000001);
+		assertEquals("Multiplication of 2147483647 with 0 is 0", 0, multiplier.multiply(2147483647, 0), 0.00000001);
+		assertEquals("Multiplication of positive infinity with 0 is undefined", Double.NaN, multiplier.multiply(Double.POSITIVE_INFINITY, 0), 0.00000001);
+		assertEquals("Multiplication of negative infinity with 6541 is undefined", Double.NEGATIVE_INFINITY, multiplier.multiply(Double.NEGATIVE_INFINITY, 6541), 0.00000001);
+		assertEquals("Multiplication of positive infinity with negative infinity is negative infinity", Double.NEGATIVE_INFINITY, multiplier.multiply(Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY), 0.00000001);
+		
+		// Test multiplying basic numbers
+		assertEquals("Multiplication of 5 with 6 is 30", 30, multiplier.multiply(5, 6), 0.00000001);
+		assertEquals("Multiplication of 27 with 16 is 432", 432, multiplier.multiply(27, 16), 0.00000001);
+	}
 }
