e7c418f743e1ead8b105202dee755f22b0437ee4
Mert Yaşin <mert.yasin@gmail.com>
2015-05-07 22:09:09 +0300

    Added doxygen style comments for subtractTest function. Fixed indentation of file.

--- TestMathematicalOperations.java
+++ TestMathematicalOperations.java
@@ -86,56 +86,60 @@ public class TestMathematicalOperations{
 		assertEquals("Sum of 5.0 with 8.0 is 13.0" , 13.0, adder.add(8.0, 5.0), 0.00000001);
 	}
 
-    /* Unit Test for power operation in MathematicalOperations class */
-    @Test
-    public void powerTest() {
-            MathematicalOperations op = new MathematicalOperations();
-            // Test for 0
-            assertEquals("0 to the power of any number is 0", 0, op.power(0,5), 0.00000001);
-            // Test for 1
-            assertEquals("1 to the power of any number is 1", 1, op.power(1,5), 0.00000001);
-            // Test for power of 0
-            assertEquals("Any number to the power of 0 is 1", 1, op.power(5,0), 0.00000001);       
-    }
+	/* Unit Test for power operation in MathematicalOperations class */
+	@Test
+	public void powerTest() {
+		MathematicalOperations op = new MathematicalOperations();
+		// Test for 0
+		assertEquals("0 to the power of any number is 0", 0, op.power(0,5), 0.00000001);
+		// Test for 1
+		assertEquals("1 to the power of any number is 1", 1, op.power(1,5), 0.00000001);
+		// Test for power of 0
+		assertEquals("Any number to the power of 0 is 1", 1, op.power(5,0), 0.00000001);       
+	}
 
-    /* Unit Test for bitwise and operation in MathematicalOperations class */
-    @Test
-    public void bitwiseAndTest() {
-            MathematicalOperations bitwiseAndOperation = new MathematicalOperations();
-			assertEquals("Bitwise and of 60 with 13 is 12.", 12, bitwiseAndOperation.bitwiseAnd(60,13),0.00000001);      
-			assertEquals("Bitwise and of 72 with 184 is 8.", 8, bitwiseAndOperation.bitwiseAnd(72,184),0.00000001);      
-		
-    }
+	/* Unit Test for bitwise and operation in MathematicalOperations class */
+	@Test
+	public void bitwiseAndTest() {
+		MathematicalOperations bitwiseAndOperation = new MathematicalOperations();
+		assertEquals("Bitwise and of 60 with 13 is 12.", 12, bitwiseAndOperation.bitwiseAnd(60,13),0.00000001);      
+		assertEquals("Bitwise and of 72 with 184 is 8.", 8, bitwiseAndOperation.bitwiseAnd(72,184),0.00000001);      
+    	}
     
-    /** Unit Test for bitwise or operation in MathematicalOperations class 
-     * Contains two JUnit test cases for bitwiseOr function in MathematicalOperations.java
-     */
-    @Test
-    public void bitwiseOrTest() {
-            MathematicalOperations bitwiseOrOperation = new MathematicalOperations();
-			assertEquals("Bitwise or of 10 with 15 is 15.", 15, bitwiseOrOperation.bitwiseOr(10, 15), 0.00000001);      
-			assertEquals("Bitwise or of 0 with 100 is 100.", 100, bitwiseOrOperation.bitwiseOr(0, 100), 0.00000001);      
-		
-    }
-     /* Unit Test for bitwise xor operation in MathematicalOperations class */
-    @Test
-    public void bitwiseXorTest() {
-            MathematicalOperations bitwiseXorOperation = new MathematicalOperations();
-			assertEquals("Bitwise or of 20 with 15 is 27.", 15, bitwiseXorOperation.bitwiseXor(20, 15), 0.00000001);      
-			assertEquals("Bitwise or of 64 with 49 is 113.", 100, bitwiseXorOperation.bitwiseXor(64, 113), 0.00000001);      
-		
-    }
-    /* Unit test for subtract function in MathematicalOperations.java */
-    @Test
-    public void subtractTest(){
-    	MathematicalOperations mo = new MathematicalOperations();
-    	assertEquals("", -Double.MAX_VALUE, mo.subtract(Double.MIN_VALUE, Double.MAX_VALUE), 0.00000001);
-    	assertEquals("", 0.0, mo.subtract(5.000000001, 5.000000000), 0.00000001);
-    	assertEquals("", 5.0, mo.subtract(12, 7), 0.00000001);
-    	assertEquals("", -12.0, mo.subtract(24, 36), 0.00000001);
-    }
+	/** Unit Test for bitwise or operation in MathematicalOperations class 
+	* Contains two JUnit test cases for bitwiseOr function in MathematicalOperations.java
+	*/
+    	@Test
+    	public void bitwiseOrTest() {
+            	MathematicalOperations bitwiseOrOperation = new MathematicalOperations();
+		assertEquals("Bitwise or of 10 with 15 is 15.", 15, bitwiseOrOperation.bitwiseOr(10, 15), 0.00000001);      
+		assertEquals("Bitwise or of 0 with 100 is 100.", 100, bitwiseOrOperation.bitwiseOr(0, 100), 0.00000001);      
+    	}
+    	
+     	/* Unit Test for bitwise xor operation in MathematicalOperations class */
+    	@Test
+    	public void bitwiseXorTest() {
+            	MathematicalOperations bitwiseXorOperation = new MathematicalOperations();
+		assertEquals("Bitwise or of 20 with 15 is 27.", 15, bitwiseXorOperation.bitwiseXor(20, 15), 0.00000001);      
+		assertEquals("Bitwise or of 64 with 49 is 113.", 100, bitwiseXorOperation.bitwiseXor(64, 113), 0.00000001);      
+	}
     
-    /* Unit Test for inverse division in MathematicalOperations class */
+	/*! \brief Unit Test function for double subtraction in MathematicalOperations class
+	 * 
+	 * Tests the edge case where the difference is out of double boundaries.
+	 * Tests the edge case where the difference is less than tolerance.
+	 * Tests some regular cases with positive and negative results.
+	 *  */
+	@Test
+	public void subtractTest(){
+		MathematicalOperations mo = new MathematicalOperations();
+		assertEquals("Double.MIN_VALUE minus Double.MAX_VALUE must be negative Double.MAX_VALUE", -Double.MAX_VALUE, mo.subtract(Double.MIN_VALUE, Double.MAX_VALUE), 0.00000001);
+		assertEquals("5.000000001 minus 5.000000000 must be zero", 0.0, mo.subtract(5.000000001, 5.000000000), 0.00000001);
+		assertEquals("12 minus 7 must be 5.0", 5.0, mo.subtract(12, 7), 0.00000001);
+		assertEquals("24 minus 36 must be -12.0", -12.0, mo.subtract(24, 36), 0.00000001);
+	}
+    
+	/* Unit Test for inverse division in MathematicalOperations class */
 	@Test
 	public void divideByInverseZero(){
 		MathematicalOperations inv = new MathematicalOperations();
