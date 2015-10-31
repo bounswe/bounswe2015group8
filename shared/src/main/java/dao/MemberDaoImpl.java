31119c55e4e249b9f73ec4f3a791235d957f10d7
Xyllan <doruketc@gmail.com>
2015-10-31 12:57:32 +0300

    Added Spring Security support. Currently using bcrypt as encryption algorithm. Added dependencies for spring security. Updated database script to make email address and username unique. Added data access object classes to remove hibernate logic from controllers.

---
+++ shared/src/main/java/dao/MemberDaoImpl.java
@@ -0,0 +1,42 @@
+package dao;
+
+import model.Member;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ * Created by xyllan on 30.10.2015.
+ */
+public class MemberDaoImpl implements MemberDao{
+    private SessionFactory sessionFactory;
+    public Member getMemberById(long id) {
+        Session s = getSessionFactory().openSession();
+        Member member = (Member)s
+                .createQuery("from Member where id=?")
+                .setParameter(0, id).uniqueResult();
+        s.close();
+        return member;
+    }
+    public Member getMemberByUsername(String username) {
+        Session s = getSessionFactory().openSession();
+        Member member = (Member)s
+                .createQuery("from Member where username=?")
+                .setParameter(0, username).uniqueResult();
+        s.close();
+        return member;
+    }
+    public Member saveMember(Member m) {
+        Session s = getSessionFactory().openSession();
+        s.getTransaction().begin();
+        s.save(m);
+        s.getTransaction().commit();
+        s.close();
+        return m;
+    }
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+}
