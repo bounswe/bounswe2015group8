b18837a17ff86a698616c6865f5f4f3977c2beb7
Muhammed Ali Arslanbenzer <muhammedarslanbenzer@gmail.com>
2015-11-28 00:35:08 +0200

    showing comments

--- android/app/src/main/java/com/cmpe/bounswe2015group8/westory/front/PostViewFragment.java
+++ android/app/src/main/java/com/cmpe/bounswe2015group8/westory/front/PostViewFragment.java
@@ -5,10 +5,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.cmpe.bounswe2015group8.westory.R;
+import com.cmpe.bounswe2015group8.westory.back.Consumer;
+import com.cmpe.bounswe2015group8.westory.back.ServerRequests;
+import com.cmpe.bounswe2015group8.westory.front.adapter.CommentAdapter;
+import com.cmpe.bounswe2015group8.westory.model.Comment;
 import com.cmpe.bounswe2015group8.westory.model.Post;
+
+import java.util.Arrays;
 
 /**
  * Fragment for viewing posts. It needs a post to be passed in as an argument.
@@ -20,8 +27,9 @@ import com.cmpe.bounswe2015group8.westory.model.Post;
  */
 public class PostViewFragment extends NamedFragment implements View.OnClickListener{
     public static final String NAME = "POST_VIEW";
-    Button btnEdit;
+    Button btnEdit, btnComment;
     TextView tvOwner, tvCreationDate, tvLastEditDate, tvContent;
+    ListView lvComments;
     Post post;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,9 +40,20 @@ public class PostViewFragment extends NamedFragment implements View.OnClickListe
         tvCreationDate = (TextView) v.findViewById(R.id.tvPostViewCreationDateValue);
         tvLastEditDate = (TextView) v.findViewById(R.id.tvPostViewLastEditDateValue);
         tvContent = (TextView) v.findViewById(R.id.tvPostViewContentValue);
+        lvComments=(ListView) v.findViewById(R.id.listComments);
         btnEdit = (Button) v.findViewById(R.id.btnPostViewEdit);
         btnEdit.setOnClickListener(this);
+        btnComment = (Button) v.findViewById(R.id.btnAddComment);
+        btnComment.setOnClickListener(this);
         initViews(this.getArguments());
+        ServerRequests sr = new ServerRequests(getActivity());
+        sr.getCommentsByPostId(post.getId(), new Consumer<Comment[]>() {
+            @Override
+            public void accept(Comment[] comments) {
+                lvComments.setAdapter(new CommentAdapter(getActivity(), R.layout.comment_small, comments));
+                post.setComments(Arrays.asList(comments));
+            }
+        });
         return v;
     }
     private void initViews(Bundle args) {
@@ -55,10 +74,18 @@ public class PostViewFragment extends NamedFragment implements View.OnClickListe
             case R.id.btnPostViewEdit:
                 NamedFragment nf = new PostEditFragment();
                 Bundle b = new Bundle();
-                b.putParcelable("post",post);
+                b.putParcelable("post", post);
                 b.putBoolean("isNew", false);
                 nf.setArguments(b);
-                MainActivity.beginFragment(getActivity(),nf);
+                MainActivity.beginFragment(getActivity(), nf);
+                break;
+
+            case R.id.btnAddComment:
+                NamedFragment nf1 = new CommentEditFragment();
+                Bundle b1 = new Bundle();
+                b1.putLong("postId", post.getId());
+                nf1.setArguments(b1);
+                MainActivity.beginFragment(getActivity(), nf1);
                 break;
         }
     }
