917b263cbde1acb0b343b78c398882a8b088f490
Göktuğ Erce Gürel <goktugercegurel@gmail.com>
2015-12-06 19:13:06 +0200

    Added heritage ids, tags and post media links to getPostById api call

--- shared/src/main/java/adapter/PostAdapter.java
+++ shared/src/main/java/adapter/PostAdapter.java
@@ -1,21 +1,27 @@
 package adapter;
 
+import api.HeritagePostUtility;
+import api.MediaUtility;
 import com.google.gson.*;
 import model.*;
-import org.apache.log4j.Logger;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 /**
  * Created by gokcan on 15.11.2015.
  */
 public class PostAdapter implements JsonSerializer<Post> {
     private CommentAdapter commentAdapter = new CommentAdapter();
+    private TagAdapter tagAdapter = new TagAdapter();
+    private MediaAdapter mediaAdapter = new MediaAdapter();
 
     @Override
     public JsonElement serialize(Post post, Type type,JsonSerializationContext jsc){
+        Gson gson = new Gson();
         JsonObject jsonObjectPost = new JsonObject();
         jsonObjectPost.addProperty("id", post.getId());
+        jsonObjectPost.addProperty("heritage_id", gson.toJson(HeritagePostUtility.getHeritageIdOfPost(post.getId())));
         jsonObjectPost.addProperty("ownerId", post.getOwner().getId());
         jsonObjectPost.addProperty("type", post.getType());
         jsonObjectPost.addProperty("postDate", post.getPostDate().toString());
@@ -45,6 +51,19 @@ public class PostAdapter implements JsonSerializer<Post> {
             comments.add(commentAdapter.serialize(comment, type, jsc));
         }
         jsonObjectPost.add("comments", comments);
+
+        JsonArray postMediasJsonArray = new JsonArray();
+        ArrayList<Media> postMedias = MediaUtility.getMedias(post.getId(), false);
+        for (Media media : postMedias) {
+            postMediasJsonArray.add(mediaAdapter.serialize(media, type, jsc));
+        }
+        jsonObjectPost.add("media", postMediasJsonArray);
+
+        JsonArray tags = new JsonArray();
+        for (Tag tag : post.getTags()) {
+            tags.add(tagAdapter.serialize(tag, type, jsc));
+        }
+        jsonObjectPost.add("tags", tags);
 
         return jsonObjectPost;
     }
